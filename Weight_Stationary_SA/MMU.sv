1bb64408c79f02ccb81cac31c0f9d7e3a945dd8a
Sidhartha <85611307+ismartsid@users.noreply.github.com>
2023-10-23 10:44:20 -0700

    Update MMU.sv

--- Weight_Stationary_SA/MMU.sv
+++ Weight_Stationary_SA/MMU.sv
@@ -1,6 +1,6 @@
 `timescale 1ns / 1ns
 
-// Systolic Array top level module. 
+// Systolic Array top-level module. 
 
 module MMU #(parameter bit_width=8, acc_width=16, size=4)
 (		input clk, input reset, input  control,
@@ -9,12 +9,7 @@ module MMU #(parameter bit_width=8, acc_width=16, size=4)
 		output [acc_width-1:0]acc_out[size:0][size-1:0],
 		output reg [acc_width-1:0]acc_out_final[size-1:0]
 );	
-	
-	
-// Implement your logic below based on the MAC unit design in MAC.v
 
-
-	
 wire [bit_width-1:0]wt_path[size:0][size-1:0];	
 
 assign acc_out[0][size-1:0] = '{default:0};
@@ -28,8 +23,6 @@ row r(.clk(clk),.reset(reset),.control(control),.data_in(data_arr[i]),.wt_path_i
 end
 endgenerate
 
-
-
 always @(posedge clk or negedge reset) begin
 if (!reset) begin
 	integer j;
@@ -42,80 +35,6 @@ if (!reset) begin
 end
 
 assign wt_path[0] = wt_arr;
-
-
-
-
-endmodule
-
-
-module row #(parameter size=4, bit_width=8, acc_width=16)(
-		input clk, input reset, input control,
-		input [bit_width-1:0]data_in,
-		input [acc_width-1:0] acc_in[size-1:0],
-		input [bit_width-1:0]wt_path_in[size-1:0], 
-		output [bit_width-1:0]wt_path_out[size-1:0],
-		output [acc_width-1:0]acc_out[size-1:0]
-);
-
-
-wire [bit_width-1:0] data[size:0];
-
-
-generate
-genvar i;
-for (i=0;i<size;i = i + 1) begin
-	MAC M(.clk(clk),.control(control),.reset(reset),.data_in(data[i]),.wt_path_in(wt_path_in[i]), 
-	.data_out(data[i+1]),.wt_path_out(wt_path_out[i]),.acc_out(acc_out[i]),.acc_in(acc_in[i]));
-end
-endgenerate
-
-assign data[0] = data_in;
-
-endmodule	
-
-
-`timescale 1ns / 1ns
-
-module MAC #(parameter bit_width=8, acc_width=16)(
-	input clk,
-	input control, // control signal used to indidate if it is weight loading or not
-	input reset,
-	input [acc_width-1:0] acc_in, // accumulation in
-	input [bit_width-1:0] data_in,  // data input or activation in
-	input [bit_width-1:0] wt_path_in,   // weight data in
-	output reg [acc_width-1:0] acc_out,  // accumulation out
-	output reg [bit_width-1:0] data_out,    // activation out
-	output [bit_width-1:0] wt_path_out		// weight data out
-	 );
-	 
-
-	// implement your MAC Unit below
-	wire [acc_width-1:0] product;
-	reg [bit_width-1:0] wt_path;
-	wire [acc_width-1:0] acc_sum;
-	
-	always @(posedge clk, negedge reset)
-	begin
-	if (!reset) begin 
-		data_out <= 0;
-		acc_out <= 0;
-		wt_path <= 0;
-		end
-	else begin
-		data_out <= data_in;
-		acc_out <= acc_sum;
-		if (control)
-			wt_path <= wt_path_in;
-		else
-			wt_path <= wt_path;
-		end
-
-  end
-
- assign product = data_in * wt_path;
- assign wt_path_out = wt_path;
- assign acc_sum = product + acc_in;
 
 endmodule
 
