ea5c8a063a907098a889917e8adc548aa6c32db5
Sidhartha <85611307+ismartsid@users.noreply.github.com>
2023-10-25 12:18:32 -0700

    Update row.sv

--- Weight_Stationary_SA/row.sv
+++ Weight_Stationary_SA/row.sv
@@ -1,12 +1,14 @@
-module row #(parameter size=16, bit_width=8, acc_width=16)(
-		input clk, input reset, input control,
-		input [bit_width-1:0]data_in,
-		input [acc_width-1:0] acc_in[size-1:0],
-		input [bit_width-1:0]wt_path_in[size-1:0], 
-		output [bit_width-1:0]wt_path_out[size-1:0],
-		output [acc_width-1:0]acc_out[size-1:0]
+module row #(parameter size=16, bit_width=8, acc_width=16)// bit_width = number of bits for each data(input); acc_width = number of bits for accumilation (typically 2*bit_width); size = size of Systolic Array
+	(
+		input clk, // Clock
+		input reset, // Negative Egde Reset
+		input control, // control signal used to indicate if it is weight loading or not, control 1 for loading weights
+		input [bit_width-1:0]data_in, // data input or activation in
+		input [acc_width-1:0] acc_in[size-1:0], // accumulation in
+		input [bit_width-1:0]wt_path_in[size-1:0],  // weight data in
+		output [bit_width-1:0]wt_path_out[size-1:0], // weight data out
+		output [acc_width-1:0]acc_out[size-1:0] // accumulation out
 );
-
 
 wire [bit_width-1:0] data[size:0];
 
